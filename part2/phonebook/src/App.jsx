2d8efe929c87dde658c4b6f55fca5b261904006f
AkshajIITD <akshaj78@gmail.com>
2024-12-10 01:07:24 +0530

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Name = (props) => {
   return(
@@ -37,18 +38,27 @@ const Persons = (props) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [isDuplicate, setIsDuplicate] = useState(false)
   const [filter, setFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }
+
+  useEffect(hook,[])
+
+  console.log('render', persons.length, 'persons')
 
   const personsToShow = filter === '' 
   ? persons
