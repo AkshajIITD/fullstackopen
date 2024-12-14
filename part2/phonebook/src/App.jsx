3d91733be625d906ed0c65d19f70d8eaaf6730c0
AkshajIITD <akshaj78@gmail.com>
2024-12-14 06:34:54 +0530

    till 2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personsService from './services/persons'
 
 const Name = (props) => {
   return(
-    <div> {props.name} {props.number} </div>
+    <div>
+       {props.name} {props.number} 
+       <button onClick={props.clickDelete} type="button"> delete </button>
+    </div>
   )
 }
 
@@ -22,7 +26,7 @@ const PersonForm = (props) => {
       <div> 
         number : <input value ={props.newNumber} onChange={props.handleNumberChange}/> </div>
       <div>
-        <button onClick={props.OnClick} type="submit" >add</button>
+        <button onClick={props.OnClick} type="submit" > add </button>
       </div>
     </form>    
   )
@@ -31,7 +35,7 @@ const PersonForm = (props) => {
 const Persons = (props) => {
  return(
   <div> {props.personsToShow.map(
-    (person) => (<Name key={person.id} name={person.name} number={person.number} />) )}
+    (person) => (<Name key={person.id} name={person.name} number={person.number} clickDelete={()=> props.clickDeleteOf(person.name,person.id)} />) )}
   </div>
  )
 }
@@ -42,6 +46,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [isDuplicate, setIsDuplicate] = useState(false)
+  const [replace, setReplace] = useState(false)
   const [filter, setFilter] = useState('')
 
   const hook = () => {
@@ -52,7 +57,7 @@ const App = () => {
       setPersons(response.data)
     }
 
-    const promise = axios.get('http://localhost:3001/persons')
+    const promise = personsService.getAll()
     promise.then(eventHandler)
   }
 
@@ -65,16 +70,18 @@ const App = () => {
   : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()) )
 
   const OnClick = () => {
-    const toFind=newName
-    const found = persons.some( (person) => person.name === toFind)
+    const personExists = persons.some( (person) => person.name.toLowerCase() === newName.toLowerCase())
     
-    if(found){
+    if(personExists){
       setIsDuplicate(true)
-      return(
-        alert(`${newName} is already added to phonebook`)
-      )
+      const confirmReplace=confirm(`${newName} is already added to phonebook, replace the old number with a new one`)
+      if(!confirmReplace){
+        setReplace(false)
+        return
+      }
+      else setReplace(true)
     }
-    setIsDuplicate(false)
+    else setIsDuplicate(false)
   }
 
   const  addName = (event) => {
@@ -83,13 +90,33 @@ const App = () => {
 
     if(!isDuplicate){
 
-      const nameObject ={
-        name: newName, number : newNumber, id : persons.length+1
+      const newObject ={
+        name: newName, number : newNumber
       }
-  
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
+
+      const eventHandler = (response) => {
+        console.log(response.data)
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')        
+      }
+      
+      const promise = personsService.create(newObject)
+      console.log("data posted")
+      promise.then(eventHandler)
+    }
+    else if(isDuplicate && replace) {
+      const exisitingPerson = persons.find( (person) => person.name.toLowerCase() === newName.toLowerCase())
+      const changedPerson = {...exisitingPerson, number : newNumber}
+
+      const eventHandler = (response) => {
+        console.log("number changed")
+        setPersons(persons.map ( (person) => person.id === exisitingPerson.id ? response.data : person))
+        setNewName('')
+        setNewNumber('')
+      }
+      const promise = personsService.update(changedPerson.id,changedPerson)
+      promise.then(eventHandler)
     }
 
   }
@@ -107,6 +134,23 @@ const App = () => {
   const handleNumberChange = (event) =>{
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const clickDeleteOf = (name,id) => {
+    const yes= confirm(`Delete ${name} ?`)
+
+    const eventHandler = () => {
+      setPersons(persons.filter(person => person.id !== id))
+      console.log(`${name} deleted successfully`)
+    }
+
+    if(yes) {
+      const promise= personsService.erase(id)
+      promise.then(eventHandler).catch(error => {
+        alert(`The person '${name}' was already deleted from the server`)
+        setPersons(persons.filter(person => person.id !== id))
+      })
+   }
   }
 
   return (
@@ -128,7 +172,7 @@ const App = () => {
 
       <h2>Numbers</h2>
 
-      <Persons personsToShow={personsToShow}/>
+      <Persons personsToShow={personsToShow} clickDeleteOf={clickDeleteOf}  />
 
     </div>
   )
